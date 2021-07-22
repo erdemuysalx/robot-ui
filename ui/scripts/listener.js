8783d1010207b4762082700fabecca60afab48d2
erd3muysal <erdemuysal@yahoo.com>
2021-07-22 21:48:16 +0300

    Updated

--- ui/scripts/listener.js
+++ ui/scripts/listener.js
@@ -1,5 +1,8 @@
 // List all topics
 // ----------------------
+var topicName = null;
+var messageType = null;
+var split = [];
 
 var topicsClient = new ROSLIB.Service({
     ros : ros,
@@ -15,55 +18,62 @@ topicsClient.callService(request, function(result) {
     //console.log('Topic Types: ', result.types);
     topicNames = result.topics;
     messageTypes = result.types;
-
-    const obj = {
-        accounting: [
-            {
-                "id"      : 999,
-                "topicName": "x",
-                "messageType" : "y",
-            },
-        ],
-    };
-
-    for (i=0; i<=topicNames.length; i++) {
-
-        var newEntry = {
-            "id" : i,
-            "topicName" : topicNames[i],
-            "messageType" : messageTypes[i], 
-        };
-
-        obj.topics.push(newEntry);
-    }
-
-
-    console.log('object: ', topics);
-
-
+    //console.log(topicNames);
+    //console.log(messageTypes);
     document.getElementById("topicnames").innerHTML = 'Topic names: ' + topicNames;
     document.getElementById("messagetypes").innerHTML = 'Message types: ' + messageTypes;
 
+    var topicAarray = [];
+
+    for (i=0; i<topicNames.length; i++) {
+        
+        var topicObject = {
+            id: "",
+            topicName: "",
+            messageType: "",
+          }
+
+        topicObject.id = i;
+        topicObject.topicName = topicNames[i];
+        topicObject.messageType = messageTypes[i];
+        topicAarray.push(topicObject);
+    }
+    //console.log(topicAarray);
+        
+    topicAarray.forEach(function(item){
+        var option = document.createElement('option');
+        option.value = item.topicName + ',' + item.messageType;
+        option.innerHTML = item.topicName;
+        selector.appendChild(option)
+    })
+    
+    selector.onchange = function(){
+        split = this.value.split(',');
+        topicName = split[0];
+        messageType = split[1];
+        console.log(split);
+    }
 });
+
 
 // Subscribing to a topic
 // ----------------------
-
 var listener = new ROSLIB.Topic({
     ros : ros,
-    name : '/chatter',
-    messageType : 'std_msgs/String'
+    name : split[0],
+    messageType : split[1],
 });
 
 listener.subscribe(function(message) {
     //document.write('Received message on ' + listener.name + ': ' + message.data + '</br>');
+    console.log(split);
+
     document.getElementById("message").innerHTML = 'Received message on ' + listener.name + ': ' + message.data;
 });
 
 
-
 /*
-// Publishing a topic
+// Publishing to a topic
 // ------------------
 
 var cmdVel = new ROSLIB.Topic({
